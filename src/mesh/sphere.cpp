7fed12bb977ed718c7a49fb14ba630d499ee9b16
Giulio Jiang <giulio.jiang14@imperial.ac.uk>
2015-08-01 11:24:13 +0100

    new sphere class with intersect method

---
+++ src/mesh/sphere.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include "geometry/vec3.hpp"
+#include "mesh/sphere.hpp"
+
+
+Sphere::Sphere(Vec3f _center, double _radius, Vec3f _surfaceColor, double _reflection,
+       double _transparency, Vec3f _emissionColor)
+{
+    center = _center;
+    radius = _radius;
+    radius2 = radius * radius;
+    surfaceColor = _surfaceColor;
+    reflection = _reflection;
+    transparency = _transparency;
+    emissionColor = _emissionColor;
+}
+
+Sphere::Sphere(Vec3f _center, double _radius, Vec3f _surfaceColor)
+{
+    center = _center;
+    radius = _radius;
+    radius2 = radius * radius;
+    surfaceColor = _surfaceColor;
+    reflection = 0;
+    transparency = 0;
+    emissionColor = Vec3f(0);
+}
+
+bool Sphere::intersect(Vec3f &rayorig, Vec3f &raydir, double &t0, double &t1)
+{
+    Vec3f l = center - rayorig;
+    double tca = l.dot(raydir);
+    if (tca < 0)
+    {
+        return false;
+    }
+
+    double d2 = l.dot(l) - tca * tca;
+    if (d2 > radius2)
+    {
+        return false;
+    }
+
+    double thc = sqrt(radius2 - d2);
+    t0 = tca - thc;
+    t1 = tca + thc;
+
+    return true;
+}
+
+
---
+++ src/mesh/sphere.hpp
@@ -0,0 +1,33 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "geometry/vec3.hpp"
+
+class Sphere
+{
+public:
+    Vec3f center;
+    double radius;
+    double radius2;
+    Vec3f surfaceColor;
+    Vec3f emissionColor;
+    double transparency;
+    double reflection;
+
+    Sphere(Vec3f _center, double _radius, Vec3f _surfaceColor, double _reflection,
+           double _transparency, Vec3f _emissionColor);
+
+    Sphere(Vec3f _center, double _radius, Vec3f _surfaceColor);
+
+    bool intersect(Vec3f &rayorig, Vec3f &raydir, double &t0, double &t1);
+
+    friend std::ostream& operator << (std::ostream &os, Sphere s)
+    {
+        os << "sphere[center=" << s.center << ",radius="
+           << s.radius << ",surfacecolor=" << s.surfaceColor
+           << ",emissionColor=" << s.emissionColor << "]";
+        return os;
+    }
+};
+
+#endif
