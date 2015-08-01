e386fdb4c505a2372b34334005eafc663b4867c7
Giulio Jiang <giulio.jiang14@imperial.ac.uk>
2015-08-01 11:23:33 +0100

    changed test verbose information macro

---
+++ src/testsuite/testutils.hpp
@@ -0,0 +1,11 @@
+#ifndef TESTUTILS_H
+#define TESTUTILS_H
+
+#define VERBOSE 1
+
+#define TESTPRINT(X) if(VERBOSE) \
+{ \
+    std::cout << X ; \
+} \
+
+#endif
--- src/testsuite/vec3test.cpp
+++ src/testsuite/vec3test.cpp
@@ -6,42 +6,42 @@
 #include <iostream>
 #include <cassert>
 
-#include "geometry/vec3.h"
-#include "testsuite/testutils.h"
+#include "geometry/vec3.hpp"
+#include "testsuite/testutils.hpp"
 
 int main()
 {
     std::cout << "Vec3 tests..." << std::endl;
     Vec3f v = Vec3f(1,2,3);
-    TESTPRINT(std::cout << v << std::endl;)
+    TESTPRINT(v << std::endl)
     assert(v.x == 1 && v.y == 2 && v.z == 3);
 
     Vec3f w = Vec3f();
-    TESTPRINT(std::cout << v + w << std::endl;)
+    TESTPRINT(v + w << std::endl)
     assert(w.x == 0 && w.y == 0 && w.z == 0);
 
     v = Vec3f(-1);
-    TESTPRINT(std::cout << v << std::endl;)
+    TESTPRINT(v << std::endl)
     assert(v.x == -1 && v.y == -1 && v.z == -1);
 
     Vec3f n = Vec3f(10,20,30);
     n.normalize();
-    TESTPRINT(std::cout << n << std::endl;)
-    TESTPRINT(std::cout << n.getNormalized() << std::endl;)
-    TESTPRINT(std::cout << "length=" << n.length() << std::endl;)
+    TESTPRINT(n << std::endl)
+    TESTPRINT(n.getNormalized() << std::endl)
+    TESTPRINT("length=" << n.length() << std::endl)
     assert(n.length() - 1 < 0.01 || n.length() - 1 > -0.01);
 
-    TESTPRINT(std::cout << n * 2.0 << std::endl;)
+    TESTPRINT(n * 2.0 << std::endl)
 
-    TESTPRINT(std::cout << (n * 2.0) * 10 << std::endl;)
+    TESTPRINT((n * 2.0) * 10 << std::endl)
 
-    TESTPRINT(std::cout << n * v << std::endl;)
+    TESTPRINT(n * v << std::endl)
 
-    TESTPRINT(std::cout << n * v * v << std::endl;)
+    TESTPRINT(n * v * v << std::endl)
 
     v = Vec3f(1,2,3);
     v *= Vec3f(-1);
-    TESTPRINT(std::cout << v << std::endl;)
+    TESTPRINT(v << std::endl)
 
     std::cout << "  OK" << std::endl << std::endl;
 
