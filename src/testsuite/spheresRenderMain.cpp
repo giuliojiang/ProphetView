258c65b9357346176748135fb9aa70284508d7c9
Giulio Jiang <giulio.jiang14@imperial.ac.uk>
2015-08-02 11:29:32 +0100

    main executable function for rendering the testing spheres image

---
+++ src/testsuite/spheresRenderMain.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <cassert>
+
+#include "geometry/vec3.hpp"
+#include "mesh/sphere.hpp"
+#include "testsuite/testutils.hpp"
+#include "integrator/directspheres.hpp"
+
+int main(int argc, char** argv)
+{
+    srand48(13);
+    std::vector<Sphere> spheres;
+    // position, radius, surface color, reflectivity, transparency, emission color
+    spheres.push_back(Sphere(Vec3f( 0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0, Vec3f(0)));
+    spheres.push_back(Sphere(Vec3f( 0.0,      0, -20),     4, Vec3f(1.00, 0.32, 0.36), 1, 0.5, Vec3f(0)));
+    spheres.push_back(Sphere(Vec3f( 5.0,     -1, -15),     2, Vec3f(0.90, 0.76, 0.46), 1, 0.0, Vec3f(0)));
+    spheres.push_back(Sphere(Vec3f( 5.0,      0, -25),     3, Vec3f(0.65, 0.77, 0.97), 1, 0.0, Vec3f(0)));
+    spheres.push_back(Sphere(Vec3f(-5.5,      0, -15),     3, Vec3f(0.90, 0.90, 0.90), 1, 0.0, Vec3f(0)));
+    // light
+    spheres.push_back(Sphere(Vec3f( 0.0,     20, -30),     3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
+    directspheres::render(spheres);
+    return 0;
+}
