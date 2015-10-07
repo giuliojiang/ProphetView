01673eb8f98427ec4c14eabacefee34ba72ca1fd
Giulio  Jiang <giulio.jiang14@imperial.ac.uk>
2015-10-07 15:22:00 +0100

    added x/y pixels and frame size members added declarations for private and public member functions

--- src/camera/cameraPerspective.hpp
+++ src/camera/cameraPerspective.hpp
@@ -1,20 +1,29 @@
 #ifndef CAMERA_PERSPECTIVE_H
 #define CAMERA_PERSPECTIVE_H
 
+#include <cmath>
+#include <iostream>
 #include "geometry/vec3.hpp"
 
 class CameraPerspective
 {
 private:
     // class members
-    int framePixelsX;
-    int framePixelsY;
+    double framePixelsX;
+    double framePixelsY;
     Vec3f originPoint;
     Vec3f viewDirection;
     Vec3f upDirection;
+    Vec3f rightDirection;
     double focalLength;
     double frameSize;
+    double frameSize_h;
+    double frameSize_v;
     // private methods
+    Vec3f getPlaneVectorX(int xcoord);
+    Vec3f getPlaneVectorY(int ycoord);
+    Vec3f getPlaneVector(int xcoord, int ycoord);
+
 public:
     // public methods
     CameraPerspective(int _framePixelsX, int _framePixelsY, Vec3f _originPoint, Vec3f _viewDirection, Vec3f _upDirection, double _focalLength, double _frameSize);
@@ -24,6 +33,8 @@ public:
         os << "[" << "framePixelsX=" << cp->framePixelsX << ",framePixelsY=" << cp->framePixelsY << ",originPoint=" << cp->originPoint << ",viewDirection=" << cp->viewDirection << ",upDirection=" << cp->upDirection << ",focalLength=" << cp->focalLength << ",frameSize=" << cp->frameSize << "]";
         return os;
     }
+    
+    Vec3f getRay(int xcoord, int ycoord);
 };
 
 #endif
